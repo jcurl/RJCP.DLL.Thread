9161b519afe115567eb0c317767bd991c01feed7
Jason Curl <jcurl@arcor.de>
2022-07-17 13:02:37 +0200

    TaskGroup: Track multiple tasks and wait when they're all complete
    Consider how to dynamically register and wait for all tasks to complete simultaneously. Tasks may be added while another thread is waiting on the group to finish (thus, extending the lifetime of the group).
    
    Issue: DOTNET-650

---
+++ code/Tasks/TaskGroup.cs
@@ -0,0 +1,176 @@
+﻿namespace RJCP.Threading.Tasks
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Maintains a task group, so that all tasks can be tracked when they're complete.
+    /// </summary>
+    public class TaskGroup : IEnumerable<Task>
+    {
+        private readonly object m_GroupLock = new object();
+        private readonly HashSet<Task> m_TaskGroup = new HashSet<Task>();
+        private readonly TaskCompletionSource<int> m_CompleteSource =
+            new TaskCompletionSource<int>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+        /// <summary>
+        /// Registers the task that is added to the list of tasks that should complete.
+        /// </summary>
+        /// <param name="task">The task that should be added to the list.</param>
+        /// <returns>
+        /// Is <see langword="true"/> if the task already exists or was added, or <see langword="false"/> otherwise
+        /// indicating that all tasks are already complete (and was not added).
+        /// </returns>
+        public bool RegisterTask(Task task)
+        {
+            lock (m_GroupLock) {
+                if (m_CompleteSource.Task.IsCompleted) return false;
+
+                if (!m_TaskGroup.Contains(task)) {
+                    m_TaskGroup.Add(task);
+                }
+            }
+
+            // We don't await, as we want this to continue running in parallel. We don't need the task, as this method
+            // sets the m_CompleteSource when everything is done.
+            _ = HandleCompletion(task);
+            return true;
+        }
+
+        /// <summary>
+        /// Registers the task that is added to the list of tasks that should complete..
+        /// </summary>
+        /// <param name="taskFunc">The task function, that returns a task if we're still running.</param>
+        /// <remarks>
+        /// Add the task returned by <paramref name="taskFunc"/> if other tasks are still being waited on. This method
+        /// avoids a race condition with <see cref="RegisterTask(Task)"/> by not requesting (and potentially starting)
+        /// the task if all existing tasks are complete.
+        /// </remarks>
+        /// <returns>
+        /// Is <see langword="true"/> if the task already exists or was added, or <see langword="false"/> otherwise
+        /// indicating that all tasks are already complete (and was not added).
+        /// </returns>
+        public bool RegisterTask(Func<Task> taskFunc)
+        {
+            Task task;
+
+            lock (m_GroupLock) {
+                if (m_CompleteSource.Task.IsCompleted) return false;
+
+                // By getting the task within 'm_GroupLock', we're sure already that we can't be completed at this point
+                // in time. Even if a task does complete, the `m_TaskGroup` won't be empty.
+                task = taskFunc();
+                if (!m_TaskGroup.Contains(task)) {
+                    m_TaskGroup.Add(task);
+                }
+            }
+
+            // We don't await, as we want this to continue running in parallel. We don't need the task, as this method
+            // sets the m_CompleteSource when everything is done.
+            _ = HandleCompletion(task);
+            return true;
+        }
+
+        private async Task HandleCompletion(Task task)
+        {
+            try {
+                await task.ConfigureAwait(false);
+                lock (m_GroupLock) {
+                    MarkCompleted(task, false);
+                }
+            } catch (OperationCanceledException) {
+                lock (m_GroupLock) {
+                    MarkCompleted(task, true);
+                    m_CompleteSource.TrySetCanceled();
+                }
+            } catch (Exception ex) {
+                lock (m_GroupLock) {
+                    MarkCompleted(task, true);
+                    m_CompleteSource.TrySetException(ex);
+                }
+            }
+
+            // We return a Task to quieten some lint tools about fire/forget async methods.
+        }
+
+        private void MarkCompleted(Task task, bool exception)
+        {
+            if (m_CompleteSource.Task.IsCompleted) return;
+
+            // We only remove tasks if we're not yet completed. It might be that a task was cancelled, or an
+            // exception was thrown, and the user needs to enumerate through the remaining tasks to cancel
+            // them, in which case, we shouldn't be modifying the collection.
+            m_TaskGroup.Remove(task);
+
+            // Check if all the tasks are completed and signal the completion source if so.
+            if (exception) FaultedTask = task;
+            if (m_TaskGroup.Count == 0 && !exception) {
+                m_CompleteSource.TrySetResult(0);
+            }
+        }
+
+        /// <summary>
+        /// Gets the faulted task.
+        /// </summary>
+        /// <value>The faulted task.</value>
+        /// <remarks>
+        /// This property is <see langword="null"/>, unless a task faulted, in which case this is only the first task
+        /// that faulted (assuming all future tasks might fault for similar reasons).
+        /// </remarks>
+        public Task FaultedTask { get; private set; }
+
+        /// <summary>
+        /// Waits for all registered tasks to complete.
+        /// </summary>
+        /// <remarks>
+        /// Tasks can be added while waiting and they're added to the wait list. If a task is added after all previous
+        /// tasks have completed, it will <b>not</b> reset the wait list.
+        /// </remarks>
+        public async Task WaitAll()
+        {
+            await m_CompleteSource.Task.ConfigureAwait(false);
+        }
+
+        private static readonly EmptyTaskList EmptyTasks = new EmptyTaskList();
+
+        private sealed class EmptyTaskList : IEnumerator<Task>
+        {
+            public Task Current { get { return null; } }
+
+            object IEnumerator.Current { get { return null; } }
+
+            public void Dispose() { /* Nothing to dispose */ }
+
+            public bool MoveNext() { return false; }
+
+            public void Reset() { /* Nothing to reset */ }
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the tasks that are registered.
+        /// </summary>
+        /// <returns>An enumerator that can be used to iterate through the collection.</returns>
+        /// <remarks>
+        /// This method is not thread safe, nor is any ordering guaranteed. You should only iterate through
+        /// the collection once an exception occurs, or a task has cancelled, to identify the remaining tasks.
+        /// If you try to enumerate while running, an empty list is returned.
+        /// </remarks>
+        public IEnumerator<Task> GetEnumerator()
+        {
+            lock (m_GroupLock) {
+                if (m_CompleteSource.Task.IsCompleted) {
+                    return m_TaskGroup.GetEnumerator();
+                }
+
+                return EmptyTasks;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ test/ThreadTest/Tasks/TaskGroupTest.cs
@@ -0,0 +1,282 @@
+﻿namespace RJCP.Threading.Tasks
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using NUnit.Framework.Constraints;
+
+    [TestFixture(UsageType.Direct)]
+    [TestFixture(UsageType.Delegates)]
+    [Timeout(5000)]
+    public class TaskGroupTest
+    {
+        public enum UsageType
+        {
+            Direct,
+            Delegates
+        }
+
+        private readonly UsageType m_UsageType;
+
+        public TaskGroupTest(UsageType usageType)
+        {
+            m_UsageType = usageType;
+        }
+
+        [Test]
+        public void DefaultTaskGroup()
+        {
+            TaskGroup group = new TaskGroup();
+            Assert.That(group.WaitAll().IsCompleted, Is.False);
+        }
+
+        public enum EventAction
+        {
+            None,
+            Cancel,
+            Exception
+        }
+
+        private static async Task WaitForEvent(string name, ManualResetEventSlim signal, EventAction eventAction = EventAction.None)
+        {
+            await Task.Run(() => {
+                Console.WriteLine($"{Environment.TickCount}: {name} Started");
+                signal.Wait();
+                Console.WriteLine($"{Environment.TickCount}: {name} Finished");
+                switch (eventAction) {
+                case EventAction.Cancel:
+                    throw new OperationCanceledException();
+                case EventAction.Exception:
+                    throw new InvalidOperationException();
+                }
+            });
+        }
+
+        [Test]
+        public async Task AddSingleTask()
+        {
+            TaskGroup group = new TaskGroup();
+
+            using (ManualResetEventSlim signal = new ManualResetEventSlim(false)) {
+                Task sigTask = null;
+                if (m_UsageType == UsageType.Direct) {
+                    sigTask = WaitForEvent("sigTask", signal);
+                    Assert.That(group.RegisterTask(sigTask), Is.True);
+                } else {
+                    Assert.That(group.RegisterTask(() => {
+                        sigTask = WaitForEvent("sigTask", signal);
+                        return sigTask;
+                    }), Is.True);
+                }
+                Assert.That(group, Is.Empty);  // Empty, because we're running.
+
+                Task sigTriggerTask = Task.Run(() => {
+                    Thread.Sleep(100);
+                    signal.Set();
+                });
+
+                await group.WaitAll();
+                Assert.That(sigTask.IsCompleted, Is.True);
+
+                await sigTriggerTask;
+                Assert.That(group, Is.Empty);  // Empty because we're finished.
+            }
+        }
+
+        [Test]
+        public async Task AddTasks()
+        {
+            TaskGroup group = new TaskGroup();
+
+            using (ManualResetEventSlim signal1 = new ManualResetEventSlim(false))
+            using (ManualResetEventSlim signal2 = new ManualResetEventSlim(false)) {
+                Task sigTask1 = null;
+                Task sigTask2 = null;
+                if (m_UsageType == UsageType.Direct) {
+                    sigTask1 = WaitForEvent("sigTask1", signal1);
+                    Assert.That(group.RegisterTask(sigTask1), Is.True);
+                    sigTask2 = WaitForEvent("sigTask2", signal2);
+                    Assert.That(group.RegisterTask(sigTask2), Is.True);
+                } else {
+                    Assert.That(group.RegisterTask(() => {
+                        sigTask1 = WaitForEvent("sigTask1", signal1);
+                        return sigTask1;
+                    }), Is.True);
+                    Assert.That(group.RegisterTask(() => {
+                        sigTask2 = WaitForEvent("sigTask2", signal2);
+                        return sigTask2;
+                    }), Is.True);
+                }
+
+                Task sigTriggerTask = Task.Run(() => {
+                    Thread.Sleep(100);
+                    signal1.Set();
+                    Thread.Sleep(100);
+                    signal2.Set();
+                });
+
+                await group.WaitAll();
+                Assert.That(sigTask1.IsCompleted, Is.True);
+                Assert.That(sigTask2.IsCompleted, Is.True);
+
+                await sigTriggerTask;
+            }
+        }
+
+        [Test]
+        public async Task AddTaskWhileWaiting()
+        {
+            TaskGroup group = new TaskGroup();
+
+            using (ManualResetEventSlim signal1 = new ManualResetEventSlim(false))
+            using (ManualResetEventSlim signal2 = new ManualResetEventSlim(false)) {
+                Task sigTask1 = null;
+                if (m_UsageType == UsageType.Direct) {
+                    sigTask1 = WaitForEvent("sigTask", signal1);
+                    Assert.That(group.RegisterTask(sigTask1), Is.True);
+                } else {
+                    Assert.That(group.RegisterTask(() => {
+                        sigTask1 = WaitForEvent("sigTask", signal1);
+                        return sigTask1;
+                    }), Is.True);
+                }
+
+                Task sigTask2 = null;
+                bool registered = false;
+
+                Task sigTriggerTask = Task.Run(() => {
+                    Thread.Sleep(100);
+
+                    // Must always register, as we register before sigTask1 is completed.
+                    if (m_UsageType == UsageType.Direct) {
+                        sigTask2 = WaitForEvent("sigTask2", signal2);
+                        registered = group.RegisterTask(sigTask2);
+                    } else {
+                        registered = group.RegisterTask(() => {
+                            sigTask2 = WaitForEvent("sigTask2", signal2);
+                            return sigTask2;
+                        });
+                    }
+                    signal1.Set();
+
+                    Thread.Sleep(100);
+                    signal2.Set();
+                });
+
+                await group.WaitAll();
+                Assert.That(sigTask1.IsCompleted, Is.True);
+                Assert.That(sigTask2.IsCompleted, Is.True);
+                Assert.That(registered, Is.True);
+
+                await sigTriggerTask;
+            }
+        }
+
+        [Test]
+        public async Task AddWhenComplete()
+        {
+            TaskGroup group = new TaskGroup();
+
+            using (ManualResetEventSlim signal = new ManualResetEventSlim(false)) {
+                Task sigTask = null;
+                if (m_UsageType == UsageType.Direct) {
+                    sigTask = WaitForEvent("sigTask", signal);
+                    Assert.That(group.RegisterTask(sigTask), Is.True);
+                } else {
+                    Assert.That(group.RegisterTask(() => {
+                        sigTask = WaitForEvent("sigTask", signal);
+                        return sigTask;
+                    }), Is.True);
+                }
+
+                Task sigTriggerTask = Task.Run(() => {
+                    Thread.Sleep(100);
+                    signal.Set();
+                });
+
+                await group.WaitAll();
+                Assert.That(sigTask.IsCompleted, Is.True);
+
+                await sigTriggerTask;
+
+                if (m_UsageType == UsageType.Direct) {
+                    Task delay = Task.Delay(10000);
+                    Assert.That(group.RegisterTask(delay), Is.False);
+                    Assert.That(delay.IsCompleted, Is.False);
+                } else {
+                    Task delay = null;
+                    Assert.That(group.RegisterTask(() => {
+                        delay = Task.Delay(10000);
+                        return delay;
+                    }), Is.False);
+                    Assert.That(delay, Is.Null);
+                }
+            }
+        }
+
+        [TestCase(EventAction.Cancel)]
+        [TestCase(EventAction.Exception)]
+        public async Task AddTasksWithError(EventAction action)
+        {
+            TaskGroup group = new TaskGroup();
+
+            using (ManualResetEventSlim signal1 = new ManualResetEventSlim(false))
+            using (ManualResetEventSlim signal2 = new ManualResetEventSlim(false)) {
+                Task sigTask1 = null;
+                Task sigTask2 = null;
+                if (m_UsageType == UsageType.Direct) {
+                    sigTask1 = WaitForEvent("sigTask1", signal1, action);
+                    Assert.That(group.RegisterTask(sigTask1), Is.True);
+                    sigTask2 = WaitForEvent("sigTask2", signal2);
+                    Assert.That(group.RegisterTask(sigTask2), Is.True);
+                } else {
+                    Assert.That(group.RegisterTask(() => {
+                        sigTask1 = WaitForEvent("sigTask1", signal1, action);
+                        return sigTask1;
+                    }), Is.True);
+                    Assert.That(group.RegisterTask(() => {
+                        sigTask2 = WaitForEvent("sigTask2", signal2);
+                        return sigTask2;
+                    }), Is.True);
+                }
+
+                Assert.That(group, Is.Empty);  // Empty, because we're running.
+
+                Task sigTriggerTask = Task.Run(() => {
+                    Thread.Sleep(100);
+                    signal1.Set();
+                    Thread.Sleep(100);
+                    signal2.Set();
+                });
+
+                ExactTypeConstraint constraint;
+                switch (action) {
+                case EventAction.Cancel:
+                    constraint = Throws.TypeOf<TaskCanceledException>();
+                    break;
+                case EventAction.Exception:
+                    constraint = Throws.TypeOf<InvalidOperationException>();
+                    break;
+                default:
+                    throw new NotSupportedException();
+                }
+
+                Assert.That(async () => {
+                    await group.WaitAll();
+                }, constraint);
+
+                Assert.That(sigTask1.IsCompleted, Is.True);
+                Assert.That(group.Count, Is.EqualTo(1));  // One task was not completed
+                Assert.That(group.FaultedTask, Is.EqualTo(sigTask1));
+
+                // The first task cancelled, the second task is in the group. The group doesn't wait for the first task
+                // to complete anymore. It is up to the user to cancel the remaining tasks.
+                Assert.That(sigTask2.IsCompleted, Is.False);
+
+                await sigTriggerTask;
+            }
+        }
+    }
+}
