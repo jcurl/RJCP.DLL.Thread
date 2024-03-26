eef5acb1be5486b1bedd4643dbbdaa5e82fdfa73
Jason Curl <jcurl@arcor.de>
2024-03-26 19:21:16 +0100

    Tasks: Set `.editorconfig` to detect missing use of `ConfigureAwait(false)`.
    Enable CA2007 to ensure that if library code uses `await`, that the use of `ConfigureAwait(false)` is correct.
    
    Add test cases to show that the `ITask` behaves correctly with `ConfigureAwait()`.
    
    Issue: DOTNET-1012

---
+++ test/ThreadTest/Tasks/ConfigureAwaitTest.cs
@@ -0,0 +1,92 @@
+﻿namespace RJCP.Threading.Tasks
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+
+    [TestFixture(false, false)]
+    [TestFixture(false, true)]
+    [TestFixture(true, false)]
+    [TestFixture(true, true)]
+    internal class ConfigureAwaitTest
+    {
+        private readonly bool m_UseITask;
+        private readonly bool m_InternalAwait;
+
+        public ConfigureAwaitTest(bool useITask, bool internalAwait)
+        {
+            m_UseITask = useITask;
+            m_InternalAwait = internalAwait;
+        }
+
+        private async ITask MyDelay(int milliseconds)
+        {
+            if (milliseconds == 0) return;
+            if (m_UseITask) {
+                await Task.Delay(milliseconds).ConfigureAwait(m_InternalAwait);
+            } else {
+                await Task.Delay(milliseconds).AsITask().ConfigureAwait(m_InternalAwait);
+            }
+        }
+
+        [TestCase(false)]
+        [TestCase(true)]
+        public async Task IsConfigureAwaitFalseMyDelay(bool withContext)
+        {
+            try {
+                Console.WriteLine($"Thread ID Before = {Environment.CurrentManagedThreadId}");
+                TestSynchronizationContext context = new();
+                SynchronizationContext.SetSynchronizationContext(context);
+                await MyDelay(10).ConfigureAwait(withContext);
+                Console.WriteLine($"Thread ID after = {Environment.CurrentManagedThreadId}");
+
+                int count = 0;
+                if (withContext) count++;
+                if (m_InternalAwait) count++;
+                Assert.That(context.PostCalled, Is.EqualTo(count));
+            } finally {
+                SynchronizationContext.SetSynchronizationContext(null);
+            }
+        }
+
+        [TestCase(false)]
+        [TestCase(true)]
+        public async Task IsConfigureAwaitFalseNoAwaitInMyDelay(bool withContext)
+        {
+            try {
+                Console.WriteLine($"Thread ID Before = {Environment.CurrentManagedThreadId}");
+                TestSynchronizationContext context = new();
+                SynchronizationContext.SetSynchronizationContext(context);
+                await MyDelay(0).ConfigureAwait(withContext);
+                Console.WriteLine($"Thread ID after = {Environment.CurrentManagedThreadId}");
+
+                int count = 0;
+                Assert.That(context.PostCalled, Is.EqualTo(count));
+            } finally {
+                SynchronizationContext.SetSynchronizationContext(null);
+            }
+        }
+
+        [TestCase(false)]
+        [TestCase(true)]
+        public async Task IsConfigureAwaitFalseMyDelayCompleted(bool withContext)
+        {
+            try {
+                Console.WriteLine($"Thread ID Before = {Environment.CurrentManagedThreadId}");
+                TestSynchronizationContext context = new();
+                SynchronizationContext.SetSynchronizationContext(context);
+                var delay = MyDelay(10).ConfigureAwait(withContext);
+                Thread.Sleep(100);
+                await delay;
+                Console.WriteLine($"Thread ID after = {Environment.CurrentManagedThreadId}");
+
+                int count = 0;
+                if (m_InternalAwait) count++;
+                Assert.That(context.PostCalled, Is.EqualTo(count));
+            } finally {
+                SynchronizationContext.SetSynchronizationContext(null);
+            }
+        }
+    }
+}
---
+++ test/ThreadTest/Tasks/TestSynchronisationContext.cs
@@ -0,0 +1,17 @@
+﻿namespace RJCP.Threading.Tasks
+{
+    using System.Threading;
+
+    internal class TestSynchronizationContext : SynchronizationContext
+    {
+        private volatile int m_PostCalled = 0;
+
+        public int PostCalled { get { return m_PostCalled; } }
+
+        public override void Post(SendOrPostCallback d, object state)
+        {
+            Interlocked.Increment(ref m_PostCalled);
+            base.Post(d, state);
+        }
+    }
+}
